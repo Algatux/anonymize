35e319bbb5c4eb15bffa98f0c720820b5ef82079
Alessandro Galli <a.galli85@gmail.com>
2024-03-30 11:51:00 +0100

    feat: added clap and basic cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    // path to the anonymize configuration file
+    #[arg(short,long)]
+    config_file: String,
+    // path to the file to anonymize
+    #[arg(short,long)]
+    source_file: Option<String>,
 }
+
+fn main() {
+    let cli = Cli::parse();
+
+    let source_file = cli.source_file.unwrap_or_default();
+
+    println!("Configuration file -> '{}'", cli.config_file);
+    println!("Source file -> '{}'", source_file);
+
+    if source_file.is_empty() {
+        println!("Empty source_file path, I will take input from stdin");
+    }
+}
+
